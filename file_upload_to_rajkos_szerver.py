307419e5ff92af3922c5dfe3493b05b7bb7f1092
Endre Mark Borza <endremborza@gmail.com>
2020-07-06 15:25:37 +0200

    Update file_upload_to_rajkos_szerver.py

--- file_upload_to_rajkos_szerver.py
+++ file_upload_to_rajkos_szerver.py
@@ -6,28 +6,19 @@ import pandas as pd
 
 df = pd.DataFrame(np.random.randint(0, 100, size=(100, 4)), columns=list("ABCD"))
 
-df.to_pickle("random_test.pkl")
+data_file_path = "random_test_v2.pkl"
 
-
-key = os.environ["RAJK_KEY"]
-
-key_file_path = "temp_key"
-
-with open(key_file_path, "w") as fp:
-    fp.write(key)
-
-subprocess.call(["chmod", "600", key_file_path])
-
-
-data_file_path = "random_test.pkl"
+df.to_pickle(data_file_path)
 
 subprocess.call(
     [
         "scp",
         "-P",
         "2222",
-        "-i",
-        key_file_path,
+        "-o", 
+        "StrictHostKeyChecking=no", 
+        "-o", 
+        "UserKnownHostsFile=/dev/null",
         data_file_path,
         "rajk@146.110.60.20:/var/www/rajk/cikkek",
     ]
